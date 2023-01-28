191a156fa11169a0b6fcb36eb09795a9341a3911
Walter Oliveira Florêncio <wt.fox@hotmail.com>
2023-01-27 21:22:09 -0300

    terraform

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Pega o ssh do provider
+data "digitalocean_ssh_key" "jornada-ssh" {
+  name = var.ssh_key_name
+}
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "jenkis" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins-vm"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.jornada-ssh.id]
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.25.4-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+variable "region" {
+  default = "nyc1"
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = " curso devops"
+}
+
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkis.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+region       = "nyc1"
+do_token     = "dop_v1_f4321d35e06910e97d47ae014c5d706bb3c9d29a2e5cb5c81cec0cbf6bf9b132"
+ssh_key_name = "curso devops"
